98353bcf7aa4f2309c39fd04eb77ec98d891e7a0
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-05-24 10:15:45 -0400

    format for larger fonts

--- Mememotron/spec/informative_failures/lists_spec.rb
+++ Mememotron/spec/informative_failures/lists_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe "A list of hash keys" do
 
   it "should be able to figure out the missing ones" do
-    expected = %w[ asof lkjwn  asdf 2klj df1o an2d oieoin laksjdf ]
-    actual   = %w[ asof lkjwn  asdf 2klj dflo an2d oieoin laksjdf ]
+    expected = %w[ asdf 2klj df1o an2d laksjdf ]
+    actual   = %w[ asdf 2klj dflo an2d laksjdf ]
 
     expect(expected).to eq(actual)
   end
