fa2ac6809f282607b225c3e140d18e89190a5229
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-05-24 10:14:15 -0400

    format for larger fonts

--- Mememotron/spec/informative_failures/dont_be_success_spec.rb
+++ Mememotron/spec/informative_failures/dont_be_success_spec.rb
@@ -2,10 +2,9 @@ require "spec_helper"
 
 describe "response codes", type: :request do
 
-  it "should tell us something use full if a request spec fails" do
+  it "should give details if a request spec fails" do
     get "/memes"
     expect(response).to be_success
   end
 
 end
-
