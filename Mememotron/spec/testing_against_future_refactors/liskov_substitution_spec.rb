56b1b4982d23321d4e2aef15b17ee806ae3d4225
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2014-05-24 10:37:41 -0400

    format for larger fonts

--- Mememotron/spec/testing_against_future_refactors/liskov_substitution_spec.rb
+++ Mememotron/spec/testing_against_future_refactors/liskov_substitution_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe "spec what you really mean, not side effects -- bad", type: :request do
+describe "don't spec side effects", type: :request do
   it "requires authentication for all indexes" do
     [ "/crypto_currencies", "/memes" ].each do |path|
       get path
@@ -9,33 +9,15 @@ describe "spec what you really mean, not side effects -- bad", type: :request do
   end
 end
 
-describe "spec what you really mean, not side effects -- good", type: :request do
+describe "spec what you mean", type: :controller do
+  controller do
+    def index
+      head :ok
+    end
+  end
 
   it "requires authentication for all indexes" do
-    get "authorized/index"
+    get :index
     expect(response.status).to eq(401)
   end
-
-  before do
-    path = "authorized/index"
-
-    # make a throw away controller
-    controller = Class.new(ApplicationController) do
-      def index
-        head :ok
-      end
-    end
-
-    # make it route-able
-    Rails.application.routes.draw do
-      get path, to: controller.action(:index)
-    end
-  end
-
-  after do
-    # clean up so the route isn't available to other tests
-    Kernel.silence_warnings { Rails.application.reload_routes! }
-  end
-
 end
-
